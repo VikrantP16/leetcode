6948187be41c384068ee2a4306b15cecd6da4275
Vikrant Patel <90693378+VikrantP16@users.noreply.github.com>
2026-01-24 22:57:50 +0530

    Time: 226 ms (51.85%) | Memory: 42.6 MB (87.98%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, i, j, num)) {
+                            board[i][j] = num;
+                            if (solve(board)) {
+                                return true;
+                            }
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        // Check row
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == num) {
+                return false;
+            }
+        }
+        
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+        
+        // Check 3x3 box
+        int boxRow = 3 * (row / 3);
+        int boxCol = 3 * (col / 3);
+        for (int i = boxRow; i < boxRow + 3; i++) {
+            for (int j = boxCol; j < boxCol + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
