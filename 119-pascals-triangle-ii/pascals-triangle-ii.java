5c03392da13b56444ccb5819976b1da0880a9871
Vikrant Patel <90693378+VikrantP16@users.noreply.github.com>
2026-07-27 23:27:03 +0530

    Time: 1 ms (69.95%) | Memory: 42.4 MB (85.51%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            row.add(1);
+        }
+
+        for (int i = 2; i <= rowIndex; i++) {
+            for (int j = i - 1; j > 0; j--) {
+                row.set(j, row.get(j) + row.get(j - 1));
+            }
+        }
+
+        return row;
+    }
+}
