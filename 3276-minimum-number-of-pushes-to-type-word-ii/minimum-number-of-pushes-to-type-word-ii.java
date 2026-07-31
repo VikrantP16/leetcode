bfec4bb8a625209034263ef33989293c2dc23e5d
Vikrant Patel <90693378+VikrantP16@users.noreply.github.com>
2026-07-31 23:23:55 +0530

    Time: 10 ms (66.76%) | Memory: 48.4 MB (17.34%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+        for (char c : word.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        int totalPushes = 0;
+        int rank = 0;
+
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) break; // no more letters actually used
+            int pushCost = (rank / 8) + 1;
+            totalPushes += freq[i] * pushCost;
+            rank++;
+        }
+
+        return totalPushes;
+    }
+}
