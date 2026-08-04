be88dfd589e8cc16a153598abb5a3f8de1fceab9
Vikrant Patel <90693378+VikrantP16@users.noreply.github.com>
2026-08-04 23:52:03 +0530

    Time: 370 ms (38.73%) | Memory: 107.5 MB (51.32%) - LeetSync

--- 464-can-i-win/can-i-win.java
+++ 464-can-i-win/can-i-win.java
@@ -2,16 +2,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    private Map<Integer, Boolean> memo = new HashMap<>();
-
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        if (desiredTotal <= 0) return true;
+
         int sum = maxChoosableInteger * (maxChoosableInteger + 1) / 2;
         if (sum < desiredTotal) return false;
 
-        return canWin(0, desiredTotal, maxChoosableInteger);
+        Map<Integer, Boolean> memo = new HashMap<>();
+        return canWin(0, desiredTotal, maxChoosableInteger, memo);
     }
 
-    private boolean canWin(int usedMask, int remaining, int maxChoosableInteger) {
+    private boolean canWin(int usedMask, int remaining, int maxChoosableInteger, Map<Integer, Boolean> memo) {
         if (memo.containsKey(usedMask)) {
             return memo.get(usedMask);
         }
@@ -19,7 +20,7 @@ class Solution {
         for (int i = 1; i <= maxChoosableInteger; i++) {
             int bit = 1 << (i - 1);
             if ((usedMask & bit) == 0) {
-                if (i >= remaining || !canWin(usedMask | bit, remaining - i, maxChoosableInteger)) {
+                if (i >= remaining || !canWin(usedMask | bit, remaining - i, maxChoosableInteger, memo)) {
                     memo.put(usedMask, true);
                     return true;
                 }
